abda88f8905a0ca05884d74319086cdc4f16875f
Piet2001 <piet2001@fireman.net>
2021-03-18 15:47:05 +0100

    add footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <body>
+          This is the app. And down here is the footer of the page!
+        </body>
+        <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>FireStationQuiz &copy;2021 - FHICT</p>
+        </footer>
+    )
+}
