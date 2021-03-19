cd5b156ec65f63ee951307c67bd5821b182e949d
Piet2001 <piet2001@fireman.net>
2021-03-19 11:41:40 +0100

    add style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer>
+        <footer className='App-footer'>
             <p>FireStationQuiz &copy;2021 - FHICT</p>
         </footer>
     )
